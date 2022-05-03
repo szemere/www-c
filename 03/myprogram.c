dc6bd4059860a1987cad31dd9083f17fbbc686a9
Laszlo Szemere <laszlo.szemere@oneidentity.com>
2022-05-03 13:30:09 +0200

    add docker example
    Signed-off-by: Laszlo Szemere <laszlo.szemere@oneidentity.com>

--- 03/myprogram.c
+++ 03/myprogram.c
@@ -1,44 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct MyList MyList;
-
-struct MyList {
-    int value;
-    MyList *next;
-};
-
-
-MyList * append(MyList *list, int data)
+void myfunc(int number)
 {
-    MyList *new_item = malloc(sizeof(MyList));
-    if (new_item)
-    {
-        new_item->next = list;
-        new_item->value = data;
-        return new_item;
-    }
-    return NULL;
-}
-
-MyList * delete(MyList *list)
-{
-    MyList *tmp = list->next;
-    free(list);
-    return tmp;
+    printf(" %d ", number);
 }
 
 int main(void)
 {
-    printf("This is my list.\n");
-    MyList *mylist = append(NULL, 42);
-    mylist = append(mylist, 1);
-    mylist = append(mylist, 2);
-    mylist = append(mylist, 3);
-
-    printf("Starting to delete items.\n");
-    mylist = delete(mylist);
-    mylist = delete(mylist);
-    mylist = delete(mylist);
-    mylist = delete(mylist);
+    printf("Hello");
+    myfunc(42);
+    printf("World!\n");
 }
